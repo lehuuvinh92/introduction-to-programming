3b74a98029c7f79f256dae33723894802ddbc36d
Vinh Le Huu <lehuuvinh92@gmail.com>
2021-09-06 15:12:43 +0700

    Add files via upload

---
+++ enum_data-type/enum_data-type.cpp
@@ -0,0 +1,28 @@
+// enum_data-type.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//output the value of members in enum data type
+	enum colors {RED, GREEN, BLUE};
+	colors b = BLUE, c = GREEN;
+	cout<<"b= "<<b<<"\n";
+	cout<<"c= "<<c<<"\n";
+
+	//an example of how to use enum
+	colors color = GREEN;
+    if (color == RED)
+        cout << "open red door";
+    else if (color == GREEN)
+		cout << "open green door";
+	else if (color == BLUE)
+		cout << "open blue door";
+	//success
+	else cout<<"close door";
+	system("pause");
+}
+
