59addafffdcd374fc9fbc488399b30d11cfa9a36
Vinh Le Huu <lehuuvinh92@gmail.com>
2021-09-06 21:22:28 +0700

    Update size-of-data-type.cpp

--- sizeof_data_type/size-of-data-type.cpp
+++ sizeof_data_type/size-of-data-type.cpp
@@ -6,12 +6,12 @@
 using namespace std; 
 int main() 
 { 
-   cout << "Kich thuoc cua char la: " << sizeof(char) << endl; 
-   cout << "Kich thuoc cua int la: " << sizeof(int) << endl; 
-   cout << "Kich thuoc cua short int la: " << sizeof(short int) << endl; 
-   cout << "Kich thuoc cua long int la: " << sizeof(long int) << endl; 
-   cout << "Kich thuoc cua float la: " << sizeof(float) << endl; 
-   cout << "Kich thuoc cua double la: " << sizeof(double) << endl; 
-   cout << "Kich thuoc cua wchar_t la: " << sizeof(wchar_t) << endl;
+   cout << "Kich thuoc cua char la: " << sizeof(char) <<" byte"<< endl; 
+   cout << "Kich thuoc cua int la: " << sizeof(int) <<" byte"<< endl; 
+   cout << "Kich thuoc cua short int la: " << sizeof(short int) <<" byte"<< endl; 
+   cout << "Kich thuoc cua long int la: " << sizeof(long int) <<" byte"<< endl; 
+   cout << "Kich thuoc cua float la: " << sizeof(float) <<" byte"<< endl; 
+   cout << "Kich thuoc cua double la: " << sizeof(double) <<" byte"<< endl; 
+   cout << "Kich thuoc cua wchar_t la: " << sizeof(wchar_t) <<" byte"<< endl;
    system("pause");
 }
