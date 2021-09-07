2f0244e0ea67a9fba2fff4095a75a64cd149b3fb
Vinh Le Huu <lehuuvinh92@gmail.com>
2021-09-07 17:22:19 +0700

    Add files via upload

---
+++ basic_input_output/cin_cout_command.cpp
@@ -0,0 +1,24 @@
+﻿// cin_cout_command.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+	char ten[50]; 
+    cout << "Input your name: "; 
+    cin >> ten; 
+    cout << "Your name is: " << ten << endl;
+
+	int a, b;
+	cout<<"Input value of a:";
+	cin>>a;
+	cout<<"Value of a = "<< a <<endl;
+	cout<<"Input value of b:";
+	cin>>b;
+	cout<<"Value of b = "<< b <<endl;
+	/* You can input a and b with one line
+	cin>>a>>b; */
+    system("pause");
+}
