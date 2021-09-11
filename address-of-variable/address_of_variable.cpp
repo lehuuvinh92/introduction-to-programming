bc600265c56ffda79b4c32be5bf40e92eadb83c4
Vinh Le Huu <lehuuvinh92@gmail.com>
2021-09-11 16:21:50 +0700

    Add files via upload
    Program to print address of variable.

---
+++ address-of-variable/address_of_variable.cpp
@@ -0,0 +1,14 @@
+﻿// address_of_variable.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream> 
+using namespace std; 
+int main() 
+{
+	int a;
+	float b;
+	cout << "Address of a: " << &a << endl;
+	cout << "Address of b: " << &b << endl;
+	system("pause");
+}
