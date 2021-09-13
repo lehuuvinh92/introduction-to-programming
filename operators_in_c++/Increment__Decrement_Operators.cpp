4d107be7fba6dffa6be3242e686bee9638e160c7
Vinh Le Huu <lehuuvinh92@gmail.com>
2021-09-13 10:00:18 +0700

    Update Increment__Decrement_Operators.cpp

--- operators_in_c++/Increment__Decrement_Operators.cpp
+++ operators_in_c++/Increment__Decrement_Operators.cpp
@@ -7,10 +7,17 @@ using namespace std;
 int main()
 {
 	int a = 10, b = 100;
-
-    cout<<"++a = "<<++a<<endl;
+	cout<<"++a = "<<++a<<endl;
 	cout<<"a++ = "<<a++<<endl;
-    cout<<"--b = "<<--b<<endl;
+	cout<<"--b = "<<--b<<endl;
 	cout<<"b-- = "<<b--<<endl;
+	cout<<"-----------------"<<endl;
+	int x = 10;
+	int y;
+	y = ++x;//x = 11 and y = 11
+	cout<<"y = ++x = "<<y<<endl;
+	x = 10;//assign x=10
+	y = x++;//y = 10 and x = 11
+	cout<<"y = x++ = "<<y<<endl;
 	system("pause");
 }
