48d6a6b3ff8290b36121e7aae3ba97dfd775d237
Vinh Le Huu <lehuuvinh92@gmail.com>
2021-09-26 15:12:01 +0700

    Update how_to_use_pointer_c++.cpp

--- pointer_in_c++/how_to_use_pointer_c++.cpp
+++ pointer_in_c++/how_to_use_pointer_c++.cpp
@@ -5,13 +5,13 @@ int main() {
 	int a = 5;
 	int *pa;
 	pa = &a;
-	//Dia chi
-	cout<<"Gia tri cua bien con tro pa la dia chi cua bien a:"<<pa<<endl;
-	cout<<"Dia chi cua bien a ma bien con tro pa tro den:"<<&a<<endl;
-	//Gia tri
-	cout<<"Gia tri vung nho ma bien con tro pa tro den:"<<*pa<<endl;
-	cout<<"Gia tri cua bien a ma bien con tro pa tro den:"<<a<<endl;
-	//Dia chi bien con tro
-	cout<<"Dia chi cua bien con tro pa:"<<&pa<<endl;
+	//Address
+	cout<<"Value of pointer pa:"<<pa<<endl;
+	cout<<"Address of variable a:"<<&a<<endl;
+	//Value
+	cout<<"Value of memory area which pointer pa point to:"<<*pa<<endl;
+	cout<<"Value of variable a:"<<a<<endl;
+	//Address of pointer
+	cout<<"Address of pointer pa:"<<&pa<<endl;
 	system("pause");
 }
